a06084af9c0672c92f4f28c6936e203f5414f9cd
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2017-07-14 09:51:26 +0800

    Skeleton for Database browser view and controller.

--- src/main/java/my/onn/jdbcadmin/MainSceneController.java
+++ src/main/java/my/onn/jdbcadmin/MainSceneController.java
@@ -1,12 +1,18 @@
 package my.onn.jdbcadmin;
 
+import java.io.IOException;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
 import my.onn.jdbcadmin.connection.ConnectionDialogController;
 
 public class MainSceneController {
@@ -29,6 +35,20 @@ public class MainSceneController {
 
         if (ConnectionDialogController.showConnectionDialog() != null) {
             Button btn = new Button("New Connection");
+            btn.setOnAction(e -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Browser.fxml"));
+
+                    Stage stage = new Stage();
+                    stage.setTitle("Database Browser");
+                    Scene scene = new Scene(loader.load());
+                    stage.setScene(scene);
+                    stage.show();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(MainSceneController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
             tilePane.getChildren().add(btn);
         }
     }
---
+++ src/main/java/my/onn/jdbcadmin/browser/BrowserController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.onn.jdbcadmin.browser;
+
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author onn
+ */
+public class BrowserController extends VBox {
+
+    /**
+     * Initializes the controller class.
+     */
+    public void initialize() {
+        // TODO
+    }
+
+}
