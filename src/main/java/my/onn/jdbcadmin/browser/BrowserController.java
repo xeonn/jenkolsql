e10a1cc506e47f5ca32b237335edb063a64187f3
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2017-08-02 10:32:05 +0800

    Initial work of splitting of ui/model code with progress bar.

--- src/main/java/my/onn/jdbcadmin/browser/BrowserController.java
+++ src/main/java/my/onn/jdbcadmin/browser/BrowserController.java
@@ -11,16 +11,26 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javax.inject.Inject;
 import my.onn.jdbcadmin.browser.sqleditor.SqlEditorWindow;
 import my.onn.jdbcadmin.connection.ConnectionModel;
@@ -102,96 +112,129 @@ public class BrowserController extends FxmlStage {
         }
     }
 
-    private void refreshTree() throws IOException {
-        try (Connection cnn = DriverManager.getConnection(model.getMaintenanceUrl(), model.getUsername(), model.getPassword())) {
+    private Stage startProgressDialog() {
+        ProgressBar pb = new ProgressBar();
+        Label label = new Label("Connecting to database, please wait ...");
+        VBox vbox = new VBox(pb, label);
+        Scene scene = new Scene(vbox);
+        Stage dialog = new Stage(StageStyle.UNDECORATED);
 
-            TreeItem rootItem = new TreeItem(String.format("%s\n[%s:%d]", model.getName(), model.getHost(), model.getPort()));
-            rootItem.setGraphic(new ImageView(new Image("icons/server_pg.png")));
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(10.0));
+        vbox.setSpacing(10.0);
+
+        dialog.centerOnScreen();
+        dialog.setAlwaysOnTop(true);
+        dialog.setScene(scene);
+        return dialog;
+    }
+
+    private CompletableFuture fetchModel() {
+        return CompletableFuture.runAsync(() -> {
+            try (Connection cnn = DriverManager.getConnection(connectionModel.getMaintenanceUrl(), connectionModel.getUsername(), connectionModel.getPassword())) {
 
                 TreeItem rootItem = new TreeItem(String.format("%s\n[%s:%d]", connectionModel.getName(), connectionModel.getHost(), connectionModel.getPort()));
                 rootItem.setGraphic(new ImageView(new Image("icons/server_pg.png")));
 
-            ResultSet rsdatabase = stmt.executeQuery();
+                // Database
+                PreparedStatement stmt = cnn.prepareStatement(
+                        "SELECT datname FROM pg_database WHERE datistemplate=false;",
+                        ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 
-            while (rsdatabase.next()) {
-                String catalog = rsdatabase.getString(1);
+                ResultSet rsdatabase = stmt.executeQuery();
 
-                TreeItem db = new TreeItem(catalog);
-                db.setGraphic(new ImageView(new Image("icons/database.png")));
+                while (rsdatabase.next()) {
+                    String catalog = rsdatabase.getString(1);
 
-                try (Connection cnnDb = DriverManager.getConnection(
-                        model.getUrl(catalog),
-                        model.getUsername(), model.getPassword())) {
+                    TreeItem db = new TreeItem(catalog);
+                    db.setGraphic(new ImageView(new Image("icons/database.png")));
 
                     try (Connection cnnDb = DriverManager.getConnection(connectionModel.getUrl(catalog),
                             connectionModel.getUsername(), connectionModel.getPassword())) {
 
-                        /* Add TABLE to schema */
-                        TreeItem ti = new TreeItem("Tables");
-                        ti.setGraphic(new ImageView(new Image("icons/notification.png")));
-                        String[] ttype = {"SYSTEM TABLE", "TABLE"};
-                        ResultSet tables = cnnDb.getMetaData().getTables(catalog, schema, null, ttype);
-                        while (tables.next()) {
+                        ResultSet schemas = cnnDb.getMetaData().getSchemas(catalog, null);
+                        while (schemas.next()) {
+                            String schema = schemas.getString(1);
+                            TreeItem si = new TreeItem(schema);
+                            si.setGraphic(new ImageView(new Image("icons/schema.png")));
 
-                            String table = tables.getString(3);
-                            TreeItem tti = new TreeItem(table);
-                            tti.setGraphic(new ImageView(new Image("icons/table-grid.png")));
+                            /* Add TABLE to schema */
+                            TreeItem ti = new TreeItem("Tables");
+                            ti.setGraphic(new ImageView(new Image("icons/notification.png")));
+                            String[] ttype = {"SYSTEM TABLE", "TABLE"};
+                            ResultSet tables = cnnDb.getMetaData().getTables(catalog, schema, null, ttype);
+                            while (tables.next()) {
 
-                            //Columns
-                            ResultSet columns = cnnDb.getMetaData().getColumns(null, null, table, null);
-                            while (columns.next()) {
+                                String table = tables.getString(3);
+                                TreeItem tti = new TreeItem(table);
+                                tti.setGraphic(new ImageView(new Image("icons/table-grid.png")));
 
-                                TreeItem ci = new TreeItem(String.format("%s [%s(%s)]",
-                                        columns.getString(4), columns.getString(6), columns.getString(7)));
-                                ci.setGraphic(new ImageView(new Image("icons/column.png")));
-                                tti.getChildren().add(ci); // Add column to tables
+                                //Columns
+                                ResultSet columns = cnnDb.getMetaData().getColumns(null, null, table, null);
+                                while (columns.next()) {
+
+                                    TreeItem ci = new TreeItem(String.format("%s [%s(%s)]",
+                                            columns.getString(4), columns.getString(6), columns.getString(7)));
+                                    ci.setGraphic(new ImageView(new Image("icons/column.png")));
+                                    tti.getChildren().add(ci); // Add column to tables
+                                }
+
+                                ti.getChildren().add(tti); // Add tables to table parent
+                            }
+                            if (!ti.getChildren().isEmpty()) // Add table parent to schema if any
+                            {
+                                si.getChildren().add(ti);
                             }
 
-                            ti.getChildren().add(tti); // Add tables to table parent
-                        }
-                        if (!ti.getChildren().isEmpty()) // Add table parent to schema if any
-                        {
-                            si.getChildren().add(ti);
-                        }
+                            /* Add VIEW to schema */
+                            TreeItem vi = new TreeItem("Views");
+                            vi.setGraphic(new ImageView(new Image("icons/screen.png")));
+                            String[] vtype = {"VIEW"};
+                            ResultSet views = cnnDb.getMetaData().getTables(catalog, schema, null, vtype);
+                            while (views.next()) {
+                                String view = views.getString(3);
+                                TreeItem vvi = new TreeItem(view);
+                                vvi.setGraphic(new ImageView(new Image("icons/open-book.png")));
 
-                        /* Add VIEW to schema */
-                        TreeItem vi = new TreeItem("Views");
-                        vi.setGraphic(new ImageView(new Image("icons/screen.png")));
-                        String[] vtype = {"VIEW"};
-                        ResultSet views = cnnDb.getMetaData().getTables(catalog, schema, null, vtype);
-                        while (views.next()) {
-                            String view = views.getString(3);
-                            TreeItem vvi = new TreeItem(view);
-                            vvi.setGraphic(new ImageView(new Image("icons/open-book.png")));
+                                //Columns
+                                ResultSet columns = cnnDb.getMetaData().getColumns(null, null, view, null);
+                                while (columns.next()) {
 
-                            //Columns
-                            ResultSet columns = cnnDb.getMetaData().getColumns(null, null, view, null);
-                            while (columns.next()) {
-
-                                TreeItem ci = new TreeItem(String.format("%s [%s(%s)]",
-                                        columns.getString(4), columns.getString(6), columns.getString(7)));
-                                ci.setGraphic(new ImageView(new Image("icons/column.png")));
-                                vvi.getChildren().add(ci); // Add column to tables
+                                    TreeItem ci = new TreeItem(String.format("%s [%s(%s)]",
+                                            columns.getString(4), columns.getString(6), columns.getString(7)));
+                                    ci.setGraphic(new ImageView(new Image("icons/column.png")));
+                                    vvi.getChildren().add(ci); // Add column to tables
+                                }
+                                vi.getChildren().add(vvi);
                             }
-                            vi.getChildren().add(vvi);
-                        }
-                        if (!vi.getChildren().isEmpty()) {
-                            si.getChildren().add(vi);
-                        }
+                            if (!vi.getChildren().isEmpty()) {
+                                si.getChildren().add(vi);
+                            }
 
-                        if (!si.getChildren().isEmpty()) // Add schema to catalog (if any)
-                        {
-                            db.getChildren().add(si);
+                            if (!si.getChildren().isEmpty()) // Add schema to catalog (if any)
+                            {
+                                db.getChildren().add(si);
+                            }
                         }
                     }
+                    rootItem.getChildren().add(db);
                 }
-                rootItem.getChildren().add(db);
-            }
 
-            treeView.setRoot(rootItem);
-        } catch (SQLException ex) {
-            Logger.getLogger(BrowserController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+                treeView.setRoot(rootItem);
+            } catch (SQLException ex) {
+                Logger.getLogger(BrowserController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+
+    private void refreshTree() throws IOException {
+        Stage dialog = startProgressDialog();
+        dialog.showAndWait();
+
+        fetchModel().thenRun(
+                () -> Platform.runLater(() -> {
+                    dialog.close();
+                }));
     }
 
     private boolean isParentRoot(TreeItem item) {
